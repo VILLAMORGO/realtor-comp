44c6d3996f734827f1cdff5229e36a5569e17c7e
VILLAMORGO <villamorgo@gmail.com>
2024-06-08 00:34:08 +0800

    Create roles for user. Admin and agent role.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_124630) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_07_161442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_124630) do
     t.string "home_address"
     t.string "city_address"
     t.string "zip_code"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
