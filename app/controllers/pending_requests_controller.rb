71f9ba3bb41048f167a00acff407fe55bd140658
VILLAMORGO <villamorgo@gmail.com>
2024-06-11 01:32:31 +0800

    verify if the user is admin for links permission

--- app/controllers/pending_requests_controller.rb
+++ app/controllers/pending_requests_controller.rb
@@ -1,5 +1,7 @@
 class PendingRequestsController < ApplicationController
 
+    before_action :verify_is_admin, only: [:index, :show, :update]
+
     def index
         @request = User.where(role: "agent", status: "Pending").where.not(confirmed_at: nil).order(:confirmed_at)
     end
@@ -15,6 +17,16 @@ class PendingRequestsController < ApplicationController
         #   UserMailer.with(user: @user).approve_email.deliver_now if params[:status] == "Approved"
         #   UserMailer.with(user: @user).decline_email.deliver_now if params[:status] == "Declined"
           redirect_to pending_requests_path, notice: "#{@user.email} has been #{@user.status}"
+        end
+    end
+
+    private
+
+    def verify_is_admin
+        if current_user.admin?
+           return
+        else
+           redirect_to root_path, notice: "You must be an admin to perform this action."
         end
     end
 end 
