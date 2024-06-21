8c790d33eb5d06786f232c07f012b2dd73b5016e
VILLAMORGO <villamorgo@gmail.com>
2024-06-22 03:33:49 +0800

    integrate mailjet for emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -71,14 +71,12 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: 'villamorgo@gmail.com',
-    password: 'your_password',
-    authentication: 'plain',
-    enable_starttls_auto: true
+  config.action_mailer.delivery_method = :mailjet_api
+  config.action_mailer.mailjet_settings = {
+    api_key: Rails.application.credentials.mailjet_api_key,
+    secret_key: Rails.application.credentials.mailjet_secret_key,
+    default_from: Rails.application.credentials.mailjet_sender_address
+  
   }
 
   # Raise error when a before_action's only/except options reference missing actions
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :mailjet_api
+  config.action_mailer.mailjet_settings = {
+    api_key: Rails.application.credentials.mailjet_api_key,
+    secret_key: Rails.application.credentials.mailjet_secret_key,
+    default_from: Rails.application.credentials.mailjet_sender_address
+  
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
