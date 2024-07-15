9f7131ed9ac10496749e291cfd8806e23b0602f8
VILLAMORGO <villamorgo@gmail.com>
2024-07-15 16:47:07 +0800

    add skip authentication on webhooks

--- app/controllers/stripe_webhooks_controller.rb
+++ app/controllers/stripe_webhooks_controller.rb
@@ -1,5 +1,6 @@
 class StripeWebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
   def create
     Rails.logger.info "Webhook created successfully"
