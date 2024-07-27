9e32b87d781e55025285ea98da8e0a19f7f300a9
VILLAMORGO <villamorgo@gmail.com>
2024-07-27 21:58:43 +0800

    Add logs to puma

--- config/puma.rb
+++ config/puma.rb
@@ -38,5 +38,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Specifies the `state` file that Puma will use.
 state_path ENV.fetch("STATE_PATH") { "tmp/pids/puma.state" }
 
+# Logging 
+stdout_redirect "/home/ubuntu/realtor-comp/log/puma.stdout.log", "home/ubuntu/realtor-comp/log/puma.stderr.log", true
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
