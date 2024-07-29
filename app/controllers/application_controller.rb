9fece8ec608ebc633f6bc375b9b227736af2bed1
VILLAMORGO <villamorgo@gmail.com>
2024-07-30 05:47:19 +0800

    refactor logic for redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_subscription, unless: :devise_controller?
-
   before_action :set_current_user, if: :user_signed_in?
 
   private
@@ -11,12 +10,12 @@ class ApplicationController < ActionController::Base
   end
 
   def check_subscription
-    # if user_signed_in? && !current_user.subscription_active? && !current_user.admin? && !on_subscription_page?
-    #   redirect_to new_subscription_path, alert: 'You must subscribe to access this page.'
-    # end
+    if user_signed_in? && !current_user.subscription_active? && !current_user.admin? && !on_subscription_page?
+      redirect_to new_subscription_path, alert: 'You must subscribe to access this page.'
+    end
   end
 
   def on_subscription_page?
-    params[:controller] == "subscriptions" && ["new", "create_checkout_session"].include?(params[:action])
+    controller_name == "subscriptions" && ["new", "create_checkout_session"].include?(action_name)
   end
 end
