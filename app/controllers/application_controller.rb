079d043d0f8298c0ca10e388162d229a6f7e63c1
VILLAMORGO <villamorgo@gmail.com>
2024-07-31 03:53:55 +0800

    uncomment check subscription in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # before_action :check_subscription, unless: :devise_controller?
+  before_action :check_subscription, unless: :devise_controller?
   before_action :set_current_user, if: :user_signed_in?
 
   private
@@ -9,13 +9,13 @@ class ApplicationController < ActionController::Base
     Current.user = current_user
   end
 
-  # def check_subscription
-  #   if user_signed_in? && !current_user.subscription_active? && !current_user.admin? && !on_subscription_page?
-  #     redirect_to new_subscription_path, alert: 'You must subscribe to access this page.'
-  #   end
-  # end
+  def check_subscription
+    if user_signed_in? && !current_user.subscription_active? && !current_user.admin? && !on_subscription_page?
+      redirect_to new_subscription_path, alert: 'You must subscribe to access this page.'
+    end
+  end
 
-  # def on_subscription_page?
-  #   controller_name == "subscriptions" && ["new", "create_checkout_session"].include?(action_name)
-  # end
+  def on_subscription_page?
+    controller_name == "subscriptions" && ["new", "create_checkout_session"].include?(action_name)
+  end
 end
