27484326427867dcfc669b30f13b98579871ac7a
VILLAMORGO <villamorgo@gmail.com>
2024-08-02 04:34:10 +0800

    allow realtor-comp.onrender.com in development env on render

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,5 +83,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.log_level = :debug
+  config.hosts << "realtor-comp.onrender.com"
 
 end
