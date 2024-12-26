9237dca062d6ac0060205b1529a094f11df003f1
VILLAMORGO <villamorgo@gmail.com>
2024-12-27 01:30:02 +0800

    Adjust the design of the subscription plan page.

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,7 @@
 module SubscriptionsHelper
+  def days_left
+    if current_user.subscription_status == "trial" && current_user.trial_ends_at.present? 
+     return current_user.trial_ends_at.to_date - Date.current  
+    end 
+  end
 end
